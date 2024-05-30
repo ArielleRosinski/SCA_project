09ad12466aa97f648c38242f8338260120cb6afe
Arielle Rosinski <ar2217@login-p-1.data.cluster>
2024-05-30 11:18:57 +0100

    altered process

--- churchland_kernel_sca.py
+++ churchland_kernel_sca.py
@@ -20,6 +20,7 @@ from absl import flags
 import sys
 
 flags.DEFINE_integer('seed', 42, 'Random seed to set')
+flags.DEFINE_integer('iterations', 10000, 'training iterations')
 flags.DEFINE_integer('d', 3, 'Subspace dimensionality')
 flags.DEFINE_string('path', '../datasets/churchland.npy',
                      'dataset path')
@@ -30,15 +31,17 @@ FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
 seed = FLAGS.seed
+iterations = FLAGS.iterations
 path = FLAGS.path 
 d = FLAGS.d
+name = FLAGS.run_name
 
 X_init = np.load(path) 
 
-X, _ = pre_processing(X_init, center=False, PCA=True)
+X, _ = pre_processing(X_init, center=False)
 X = jnp.array(X)
 K, N, T = X.shape
-A = jnp.swapaxes(pre_processing(X_init, PCA=True)[0], 0, 1)       #(N, K, T)
+A = jnp.swapaxes(pre_processing(X_init)[0], 0, 1)       #(N, K, T)
 A = A.reshape(N,-1)                                                #(N, K*T)
 
 def K_X_Y_diagonal(X, Y, sigma_sqrd):
@@ -102,7 +105,7 @@ def update(alpha_tilde, P, S, K_A_X, X, d, optimizer, opt_state, key):
     alpha_tilde_updated = optax.apply_updates(alpha_tilde, updates)
     return alpha_tilde_updated, opt_state_updated
 
-def optimize(P, S, K_A_X, X, iterations=100, learning_rate=0.01, d=3, seed=42):
+def optimize(P, S, K_A_X, X, iterations=10000, learning_rate=0.001, d=3, seed=42):
     K, N, T = X.shape
     key = random.PRNGKey(seed)
     
@@ -133,7 +136,7 @@ def optimize(P, S, K_A_X, X, iterations=100, learning_rate=0.01, d=3, seed=42):
     return alpha_tilde, ls_loss, ls_S_ratio
 
 
-wandb.init(project="SCA-project-kernel", name=f"{FLAGS.name}", mode="online")
-optimized_alpha_tilde, ls_loss = optimize(P, S, K_A_X, X, seed = seed )
+wandb.init(project="SCA-project-kernel", name=name, mode="online")
+optimized_alpha_tilde, _,  _ = optimize(P, S, K_A_X, X, iterations= iterations, seed = seed )
 wandb.finish()
-np.save(f"{FLAGS.name}", optimized_alpha_tilde)
+np.save(f'../outputs/{name}', optimized_alpha_tilde)
