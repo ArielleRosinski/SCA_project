0cc1c4ceb6c6136d33656a24f97731593f039856
Arielle Rosinski <ar2217@login-p-4.data.cluster>
2024-06-03 15:08:05 +0100

    kernel test

--- churchland_kernel_sca.py
+++ churchland_kernel_sca.py
@@ -23,7 +23,7 @@ flags.DEFINE_integer('seed', 42, 'Random seed to set')
 flags.DEFINE_integer('iterations', 10000, 'training iterations')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_integer('d', 3, 'Subspace dimensionality')
-flags.DEFINE_string('path', '/rds/user/ar2217/hpc-work/SCA/datasets/churchland.npy',
+flags.DEFINE_string('path', '/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/churchland.npy',
                      'dataset path')
 flags.DEFINE_string('save_path', '/rds/user/ar2217/hpc-work/SCA/outputs/Kernel',
                      'save path')
@@ -47,17 +47,6 @@ name = FLAGS.name
 save = FLAGS.save
 pre_processing_ = FLAGS.pre_processing_
 
-if pre_processing_:
-    X_init = np.load(path) 
-    X, _ = pre_processing(X_init, center=False,soft_normalize='max')
-    K, N, T = X.shape       
-    A = jnp.swapaxes(pre_processing(X_init,soft_normalize='max')[0], 0, 1)       #(N, K, T)
-    A = A.reshape(N,-1)                                                          #(N, K*T)
-else:
-    X = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/X_softNormMax.npy')
-    K, N, T = X.shape
-    A = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/A_softNormMax.npy')
-
 def K_X_Y_diagonal(X, Y, sigma_sqrd):
     """For two spatial patterns X and Y, the kernel k(x_i,y_i) is equal to sum_i sigma_i^2 x_i y_i"""
     return jnp.dot(X.T * sigma_sqrd, Y) 
@@ -65,24 +54,37 @@ def K_X_Y_diagonal(X, Y, sigma_sqrd):
 def K_X_Y_identity(X, Y):
     return jnp.dot(X.T, Y) 
 
+if pre_processing_:
+    print(f'pre_processing_ is True')
+    X_init = np.load(path) 
+    X, _ = pre_processing(X_init, center=False,soft_normalize='max')
+    K, N, T = X.shape       
+    A = jnp.swapaxes(pre_processing(X_init,soft_normalize='max')[0], 0, 1)       #(N, K, T)
+    A = A.reshape(N,-1)                                                          #(N, K*T)
+    
+    K_A_X = K_X_Y_identity(A, X)                                                 #(K*T, K, T)
+    K_A_A = K_X_Y_identity(A, A)
+    K_A_A_reshaped = K_A_A.reshape(K,T,K,T)                                      #(K,T,K,T)
+    means = jnp.mean(K_A_A_reshaped, axis=(0, 2), keepdims=True)                 #(1, T, 1, T)
+    K_A_A_tilde = (K_A_A_reshaped - means).reshape(K*T,K*T)                      #(K*T,K*T)
+    P, S, Pt = jnp.linalg.svd(K_A_A_tilde, full_matrices=False)                  #P is (K*T, K*T) and S is (K*T,)
+else:
+    print(f'pre_processing_ is False')
+    X = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/X_centerFalse_softNormMax.npy')
+    K, N, T = X.shape
+    A = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/A_softNormMax.npy')
 
-K_A_X = K_X_Y_identity(A, X)                                    #(K*T, K, T)
-
-K_A_A = K_X_Y_identity(A, A)
-K_A_A_reshaped = K_A_A.reshape(K,T,K,T)                          #(K,T,K,T)
-means = jnp.mean(K_A_A_reshaped, axis=(0, 2), keepdims=True)     #(1, T, 1, T)
-K_A_A_tilde = (K_A_A_reshaped - means).reshape(K*T,K*T)          #(K*T,K*T)
-P, S, Pt = jnp.linalg.svd(K_A_A_tilde, full_matrices=False)      #P is (K*T, K*T) and S is (K*T,)
+    K_A_X = K_X_Y_identity(A, X)                                    #(K*T, K, T)
+    K_A_A = K_X_Y_identity(A, A)
+    P = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/P_centerFalse_softNormMax.npy')
+    S = np.load('/rds/user/ar2217/hpc-work/SCA/datasets/Churchland/S_centerFalse_softNormMax.npy')
 
 def single_pair_loss(alpha_H, K_A_X, id_1, id_2, operator = 'minus'):
     K_A_X_i = K_A_X[:,id_1,:]
     K_X_A_i = K_A_X[:,id_2,:].T
-    
-    #Q = alpha_H.T @ K_A_X_i @ K_X_A_i @ alpha_H                         #(KT,D).T @ (KT,T) and (T,KT) @ (KT,D) --> (D,T) @ (T,D) --> (D,D)
-    Q = jnp.einsum('kd,kt,tj,jm->dm', alpha_H, K_A_X_i, K_X_A_i, alpha_H)
-    #QQ_product = jnp.einsum('ij,lm->im', Q, Q)
+                  
+    Q = jnp.einsum('kd,kt,tj,jm->dm', alpha_H, K_A_X_i, K_X_A_i, alpha_H)    #(KT,D).T @ (KT,T) and (T,KT) @ (KT,D) --> (D,T) @ (T,D) --> (D,D)
     QQ_product = Q @ Q
-
 
     if operator == 'minus':
         return jnp.trace(Q)**2 - jnp.trace(QQ_product)
@@ -153,10 +155,23 @@ def optimize(P, S, K_A_X, X, iterations=10000, learning_rate=0.001, d=3, seed=42
     return alpha_tilde, ls_loss, ls_S_ratio
 
 
-wandb.init(project="SCA-project-kernel", name=name, mode="online")
+wandb.init(project="SCA-project-kernel", name=name, mode="disabled")
 optimized_alpha_tilde, _,  _ = optimize(P, S, K_A_X, X, iterations= iterations, learning_rate= learning_rate, seed = seed )
 wandb.finish()
 
 if save: 
     np.save(f'{save_path}/{name}', optimized_alpha_tilde)
 
+alpha_tilde_QR, _ = jnp.linalg.qr(optimized_alpha_tilde) 
+alpha = (P / jnp.sqrt(S)) @ alpha_tilde_QR
+
+alpha_reshaped = alpha.reshape(K,T,d)                                           #(K, T, d)
+mean = jnp.mean(alpha_reshaped, axis=(0), keepdims=True)                        #(1, T, d)
+optimized_alpha_H = (alpha_reshaped - mean).reshape(K*T,d)                      #(K*T,d)
+projection = jnp.einsum('ij,imk->mjk', optimized_alpha_H, K_A_X)                #(K*T,d) @ (K*T, K, T) --> (K, d, T)
+
+np.save(f'{save_path}/{name}_P.2', P)
+np.save(f'{save_path}/{name}_S.2', S)
+np.save(f'{save_path}/{name}_proj', projection)
+plot_3D(projection)
+plt.savefig(f'{save_path}/{name}_fig')
