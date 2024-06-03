201865bef463a4eebd85877bf1ba078801a4ea70
Arielle Rosinski <ar2217@login-p-2.data.cluster>
2024-06-03 10:18:00 +0100

    clust test 2.1

--- MC_Maze_linear_SCA.py
+++ MC_Maze_linear_SCA.py
@@ -27,10 +27,13 @@ import sys
 
 flags.DEFINE_integer('seed', 42, 'Random seed to set')
 flags.DEFINE_integer('d', 3, 'Subspace dimensionality')
-flags.DEFINE_string('dataset_path', "../datasets/MC_Maze/psth.npy",
+flags.DEFINE_integer('iterations', 5000, 'training iterations')
+flags.DEFINE_string('dataset_path', "/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze/psth.npy",
                      'dataset path')
-flags.DEFINE_string('save_path', "../outputs/MC_Maze",
+flags.DEFINE_string('save_path', "/rds/user/ar2217/hpc-work/SCA/outputs/MC_Maze",
                      'dataset path')
+flags.DEFINE_boolean('pre_processing_', False,
+                     'Use pre_processing function versus load pre-processed X')
 
 
 FLAGS = flags.FLAGS
@@ -40,19 +43,32 @@ dataset_path = FLAGS.dataset_path
 save_path = FLAGS.save_path 
 d = FLAGS.d
 seed = FLAGS.seed
-
+iterations = FLAGS.iterations
+pre_processing_ = FLAGS.pre_processing_
 
 X_raw = np.load(dataset_path).swapaxes(1,2)
 K, N, T = X_raw.shape
 
-X, _ = pre_processing(X_raw, soft_normalize='max')
-X = jnp.array(X)
-X_pre_pca, _ = pre_processing(X_raw, soft_normalize='max', pca=False)
+if pre_processing_:
+    X, _ = pre_processing(X_raw, soft_normalize='max')
+    X_pre_pca, _ = pre_processing(X_raw, soft_normalize='max', pca=False)
+else: 
+    X = np.load("/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze/X_softNormMax.npy")
+    X_pre_pca = np.load("/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze/X_softNormMax_pcaFalse.npy")
 
-wandb.init(project="SCA-project-MC_Maze", name=f"d={d}.5", mode="online")
-U, ls_loss, ls_S_ratio = optimize(X,d=d, seed=seed) 
+wandb.init(project="SCA-project-MC_Maze", name=f"d={d}.5", mode="disabled")
+U, ls_loss, ls_S_ratio = optimize(X,d=d, seed=seed, iterations=100) 
 np.save(f'{save_path}/U_psth_{d}d', U)
 wandb.finish()
+
+U_qr, _ = jnp.linalg.qr(U)        
+Y = jnp.einsum('ji,kjl->kil', U_qr, X)
+plot_3D(Y[:,:,:])
+plt.title(f"sca {var_explained(X, U_qr):.2f}")
+# plt.savefig(f'{save_path}/proj_{d}d')
+# np.save(f'{save_path}/Y_{d}d', Y)
+np.save(f'{save_path}/X_{d}d_2', X)
+np.save(f'{save_path}/Xraw_{d}d', X_raw)
 
 X_reshaped = np.concatenate(X_pre_pca.swapaxes(1,2))
 pca = PCA(d)
@@ -61,6 +77,10 @@ PCs = pca.components_
 X_pca = X_pca.reshape(K, T, d).swapaxes(1,2)
 pca_variance_captured = pca.explained_variance_ratio_
 
-np.save(f'{save_path}/pca_variance_captured_{d}d', pca_variance_captured)
-np.save(f'{save_path}/PCs_{d}d', PCs)
-np.save(f'{save_path}/X_pca_{d}d', X_pca)
+# plt.figure()
+# plot_3D(X_pca[:,:,:])
+# plt.savefig(f'{save_path}/proj_pca_{d}d')
+
+# np.save(f'{save_path}/pca_variance_captured_{d}d', pca_variance_captured)
+# np.save(f'{save_path}/PCs_{d}d', PCs)
+# np.save(f'{save_path}/X_pca_{d}d', X_pca)
