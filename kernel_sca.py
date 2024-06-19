34a091888e1f005a96ab1b64e09ac20618da05de
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-06-19 09:31:10 +0100

    pre inducing points

--- kernel_sca.py
+++ kernel_sca.py
@@ -11,17 +11,6 @@ import wandb
 
 from itertools import combinations
 
-def K_X_Y_diagonal(X, Y, sigma_sqrd):
-    """For two spatial patterns X and Y, the kernel k(x_i,y_i) is equal to sum_i sigma_i^2 x_i y_i"""
-    return jnp.dot(X.T * sigma_sqrd, Y) 
-
-def K_X_Y_identity(X, Y):
-    return jnp.dot(X.T, Y) 
-
-def K_X_Y_squared_exponential(X, Y, l=1.0, sigma_f=1.0):
-    sq_dist = jnp.sum((X.T[:, jnp.newaxis, :] - Y.T[jnp.newaxis, :, :])**2, axis=2)
-    return sigma_f**2 * jnp.exp(-0.5 / l**2 * sq_dist)
-
 @jit
 def qr_decomposition(A):
     m, n = A.shape
