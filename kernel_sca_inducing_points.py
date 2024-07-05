cc8d0014ce65edd1d8409bf560c3949213325324
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-07-05 12:36:19 +0100

    toy datasets 2

--- kernel_sca_inducing_points.py
+++ kernel_sca_inducing_points.py
@@ -56,14 +56,14 @@ def get_alpha(params, A, X, kernel_function, d):
 
     K_u_u_K_u_A_alpha_H =  jnp.einsum('ij,jm->im',  K_u_u_K_u_A, alpha_H)    #(c, KT) @ (KT, d) --> (c, d)                       
     
-    return  K_u_u_K_u_A_alpha_H
+    return  K_u_u_K_u_A_alpha_H, K_A_u, K_u_u
  
 def loss(params, X, A, d,kernel_function, key, normalized = False):  
     K, N, T = X.shape
 
     _, u, l2, scale = get_params(params, kernel_function)
 
-    K_u_u_K_u_A_alpha_H = get_alpha(params, A, X, kernel_function, d) 
+    K_u_u_K_u_A_alpha_H, _, _ = get_alpha(params, A, X, kernel_function, d) 
 
 
     num_pairs = 100  
