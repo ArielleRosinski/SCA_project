b6d440c56291806fb4c1afb51e883ab00b491247
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-07-06 20:41:23 +0100

    run R2

---
+++ R2_nonlinear.py
@@ -0,0 +1,86 @@
+import numpy as np 
+
+import optax
+
+import math
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from utils import *
+
+import wandb
+
+import pickle
+
+from absl import app
+from absl import flags
+import sys
+
+import os
+
+import jax
+import jax.numpy as jnp
+from jax import grad, random, vmap
+import optax
+
+from sklearn.linear_model import RidgeCV
+from sklearn.metrics import r2_score
+
+from NN_jax import *
+
+
+flags.DEFINE_string('path_X', '',
+                     'kSCA/SCA/PCA-projected data')
+flags.DEFINE_string('path_Y', '/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze_20ms/behaviour/hand_vel.npy',
+                     'behaviour')
+flags.DEFINE_string('save_path', '/rds/user/ar2217/hpc-work/SCA/outputs/motor_cortex/R2_nonlinear/hand_vel',
+                     'save path')
+flags.DEFINE_string('name', '',
+                     'file name')
+flags.DEFINE_integer('d', 3, 'subspace dimensionality')
+flags.DEFINE_integer('split', 10, 'test/train split')
+flags.DEFINE_integer('lag', 5, 'behaviour/neural prediction time lag')
+flags.DEFINE_integer('iterations', 10000, 'training iterations')
+
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
+path_X = FLAGS.path_X
+path_Y = FLAGS.path_Y
+save_path = FLAGS.save_path
+name = FLAGS.name
+d = FLAGS.d
+iterations = FLAGS.iterations
+split = FLAGS.split
+lag = FLAGS.lag
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+behaviour = np.load(path_Y)
+y_train = behaviour[split:,:,lag:].swapaxes(1,2).reshape(-1, behaviour.shape[1])
+y_test = behaviour[:split,:,lag:].swapaxes(1,2).reshape(-1, behaviour.shape[1])
+
+X = np.load(path_X)
+X_train = X[split:,:,:-lag].swapaxes(1,2).reshape(-1, X.shape[1])
+X_test = X[:split,:,:-lag].swapaxes(1,2).reshape(-1, X.shape[1])
+
+params, ls_loss = optimize(X_train, y_train, layer_sizes = [d, 10, 2], num_iterations = iterations, seed=42, learning_rate = 1e-1)
+
+predictions = predict(params, X_test)
+r2 = r2_score(y_test, predictions)
+
+np.save(f'{save_path}/{name}_{d}_ls_loss', np.array(ls_loss))
+
+plt.figure()
+plt.plot(ls_loss)
+plt.savefig(f'{save_path}/{name}_{d}_loss_fig.png')
+
+#np.save(f'{save_path}/{name}_{d}_params', np.array(params))
+with open(f"{save_path}/{name}_{d}_params.pkl", "wb") as file:
+    pickle.dump(params, file)
+
+np.save(f'{save_path}/{name}_{d}_predictions', np.array(predictions))
+
+np.save(f'{save_path}/{name}_{d}_r2 ', np.array(r2))
---
+++ cluster_launch/run_R2_nonlinear.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -J mlmi-sca
+#SBATCH -A MLMI-ar2217-SL2-GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --array=1-27
+#SBATCH --output=logs_R2/R2_%A_%a.out
+#SBATCH -p ampere
+
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp              # REQUIRED - loads the basic environment
+
+# Commands for running JAX
+module avail nvidia-cuda-toolkit
+export LD_LIBRARY_PATH=~/.conda/envs/sca_env/lib:$LD_LIBRARY_PATH
+export PATH=~/.conda/envs/sca_env/bin:$PATH
+
+
+source /home/${USER}/.bashrc
+conda activate sca_env
+export OMP_NUM_THREADS=1
+
+
+config=/rds/user/ar2217/hpc-work/SCA/SCA_project/cluster_launch/R2_nonlinear_params.txt
+
+d=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+path_X=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+name=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' $config)
+
+python /rds/user/ar2217/hpc-work/SCA/SCA_project/R2_nonlinear.py --d $d --path_X $path_X --name $name
+
