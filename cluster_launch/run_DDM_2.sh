0f48a62928a6806a031fa06a6bf8f4abf53a31c2
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-08-01 11:27:18 +0200

    ddm

---
+++ cluster_launch/run_DDM_2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -J mlmi-sca
+#SBATCH -A MLMI-ar2217-SL2-GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --array=1
+#SBATCH --output=logs_DDM/DDM_%A_%a.out
+#SBATCH -p ampere
+
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp              # REQUIRED - loads the basic environment
+
+# Commands for running JAX
+module avail nvidia-cuda-toolkit
+export LD_LIBRARY_PATH=~/.conda/envs/sca_env/lib:$LD_LIBRARY_PATH
+export PATH=~/.conda/envs/sca_env/bin:$PATH
+
+
+source /home/${USER}/.bashrc
+conda activate sca_env
+export OMP_NUM_THREADS=1
+
+
+config=/rds/user/ar2217/hpc-work/SCA/SCA_project/cluster_launch/DDM_params.txt
+
+d=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+sigma_noise=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+l2_=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' $config)
+proj_dims=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $5}' $config)
+
+python /rds/user/ar2217/hpc-work/SCA/SCA_project/DDM.py --d $d --sigma_noise $sigma_noise --l2_ $l2_ --proj_dims $proj_dims
+
