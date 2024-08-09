a926a02b7f25c11797d451565b86fcc3a5873a88
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-08-09 13:46:46 +0200

    analyse mc maze

--- cluster_launch/run_var_explained.sh
+++ cluster_launch/run_var_explained.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=NONE
-#SBATCH --array=1-18
+#SBATCH --array=1-9
 #SBATCH --output=logs_var_exp/var_exp_%A_%a.out
 #SBATCH -p ampere
 
--- get_var_explained_single_trials.py
+++ get_var_explained_single_trials.py
@@ -32,18 +32,14 @@ elif kernel == 'linear':
 elif kernel == 'rational_quadratic':
     kernel_function = K_X_Y_rational_quadratic
 
-#A_train = jnp.load('/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze_20ms/A_softNormMax_centerFalse_spikes.npy')
-#X_train = jnp.load('/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze_20ms/X_softNormMax_centerFalse_spikes.npy')
-A_train = jnp.load('/Users/ariellerosinski/My Drive/Cambridge/Project/datasets/MC_Maze/A_softNormMax_centerFalse_spikes.npy')
-X_train = jnp.load('/Users/ariellerosinski/My Drive/Cambridge/Project/datasets/MC_Maze/X_softNormMax_centerFalse_spikes.npy')
+A_train = jnp.load('/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze_20ms/A_softNormMax_centerFalse_pcaFalse.npy')
+X_train = jnp.load('/rds/user/ar2217/hpc-work/SCA/datasets/MC_Maze_20ms/A_softNormMax_centerFalse_pcaFalse.npy')
 
-#params = jnp.load(f'/rds/user/ar2217/hpc-work/SCA/outputs/MC_Maze/neural_spikes/params_{d}d_{kernel}.npy', allow_pickle=True).tolist()
-params = jnp.load(f'/Users/ariellerosinski/My Drive/Cambridge/Project/outputs/kernel_SCA/MC_Maze_3_7_2024/kernel_/neural_spikes/params_{d}d_{kernel}.npy', allow_pickle=True).tolist()
+
+params = jnp.load(f'/rds/user/ar2217/hpc-work/SCA/outputs/MC_Maze/neural_spikes_kernel_pcaFalse/params_{d}d_{kernel}.npy', allow_pickle=True).tolist()
 
 _, _, _, _, alpha  = get_alpha(params, A_train, X_train, kernel_function, d)
 _, _, l2, scale = get_params(params, kernel_function=kernel_function)
 
 var = var_explained_kernel(alpha, kernel_function, A_train, X_train, l2, scale, compute_matrix=False)
-print(var)
-#np.save(f'/rds/user/ar2217/hpc-work/SCA/outputs/MC_Maze/var_explained_spikes/var_{d}d_{kernel}', var)
-np.save(f'/Users/ariellerosinski/My Drive/Cambridge/Project/outputs/kernel_SCA/MC_Maze_3_7_2024/results/R2/spikes_hand_vel/var_explained_spikes/var_{d}d_{kernel}', var)
+np.save(f'/rds/user/ar2217/hpc-work/SCA/outputs/MC_Maze/var_explained_spikes/var_{d}d', var)
